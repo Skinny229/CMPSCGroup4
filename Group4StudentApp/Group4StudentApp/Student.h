9cb451fe52e3d8c7dae9b7772113ef8c83e4bfd4
skinny229 <Skinny229@gmail.com>
2019-02-11 13:54:13 -0500

    Added Setters and Getters

--- Group4StudentApp/Group4StudentApp/Student.h
+++ Group4StudentApp/Group4StudentApp/Student.h
@@ -41,6 +41,9 @@ namespace SpaghettiLizards
 		//Semester
 		Semester startSemester;
 
+		//Current Status
+		string status;
+
 	public:
 
 		/*
@@ -58,11 +61,129 @@ namespace SpaghettiLizards
 		 */
 		Student(string,string,string,int,int,MailingAddress,Email,PhoneNumber,Date,Date);
 
-		//Setters
+	
+		
 
-		//Getters
+		//GETTER and SETTERS by constructor order
+		string getFirstName() const
+		{
+			return firstName;
+		}
 
+		void setFirstName(string firstName)
+		{
+			this->firstName = firstName;
+		}
 
+		string getMiddleName() const
+		{
+			return middleName;
+		}
+
+		void setMiddleName(string middleName)
+		{
+			this->middleName = middleName;
+		}
+
+		string getLastName() const
+		{
+			return lastName;
+		}
+
+		void setLastName(string lastName)
+		{
+			this->lastName = lastName;
+		}
+
+		int getId() const
+		{
+			return id;
+		}
+
+		void setId1(int id)
+		{
+			this->id = id;
+		}
+
+		int getUserId() const
+		{
+			return userId;
+		}
+
+		void setUserId1(int userId)
+		{
+			this->userId = userId;
+		}
+
+		string getIntendedMajor() const
+		{
+			return intendedMajor;
+		}
+
+		void setIntendedMajor(string intendedMajor)
+		{
+			this->intendedMajor = intendedMajor;
+		}
+
+		string getIntendedMinor() const
+		{
+			return intendedMinor;
+		}
+
+		void setIntendedMinor(string intendedMinor)
+		{
+			this->intendedMinor = intendedMinor;
+		}
+
+		MailingAddress getAddress() const
+		{
+			return address;
+		}
+
+		void setAddress(MailingAddress address)
+		{
+			this->address = address;
+		}
+
+		Date getBirthDate() const
+		{
+			return birthDate;
+		}
+
+		void setBirthDate(Date birthDate)
+		{
+			this->birthDate = birthDate;
+		}
+
+		Date getAcceptedDate() const
+		{
+			return acceptedDate;
+		}
+
+		void setAcceptedDate(Date acceptedDate)
+		{
+			this->acceptedDate = acceptedDate;
+		}
+
+		Semester getStartSemester() const
+		{
+			return startSemester;
+		}
+
+		void setStartSemester(Semester startSemester)
+		{
+			this->startSemester = startSemester;
+		}
+
+		string getStatus() const
+		{
+			return status;
+		}
+
+		void setStatus(string status)
+		{
+			this->status = status;
+		}
 	};
 
 }

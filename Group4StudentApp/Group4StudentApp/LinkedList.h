b60d44b564b008a01efa78a678ad8711900af05b
Rodrigo Lopez <skinny229@gmail.com>
2019-03-12 14:03:41 -0400

    Added LinkedList class definition

---
+++ Group4StudentApp/Group4StudentApp/LinkedList.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef LinkedList_H
+#define LinkedList_H
+
+namespace SpaghettiLizzards 
+{
+	template<class ListType>
+	class LinkedList 
+	{
+	private:
+		ListType* head_ptr;
+	public:
+
+	};
+}
+
+
+#endif

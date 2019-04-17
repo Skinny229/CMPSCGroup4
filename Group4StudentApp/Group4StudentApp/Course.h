7a6b6cf86bba04d9354cb8dc582afbccaa9aab14
SSnowyy <mlglenn@comcast.net>
2019-04-17 16:47:24 -0400

    Create Course.h

---
+++ Group4StudentApp/Group4StudentApp/Course.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#ifndef COURSE_H
+#define COURSEs_H
+
+using namespace std;
+
+namespace SpaghettiLizards
+{
+	class Course
+	{
+	private:
+		int courseNum;
+		string courseDef;
+		string semesterTaken;
+		string currentStatus;
+		string letterGade;
+	public:
+		Course(int = 0, string = "?", string = "?", string = "?", string = "?");
+
+		void getCourseFromFile(string);
+		void writeCourseToFile(ostream&);
+
+
+		void setCourseNum(int);
+		void setCourseDef(string);
+		void setSemesterTaken(string);
+		void currentStatus(string);
+		void setLetterGrade(string);
+
+		int getCourseNum() const;
+		string getCourseDef() const;
+		string getSemesterTaken() const;
+		string getCurrentStatus() const;
+		string getLetterGrade() const;
+	};
+}
+#endif
